e79ecb4089bc4a510e823f61c8cc2ad7d06b096d
Heloiisee <heloiseguicheron71@gmail.com>
2025-01-21 21:37:51 -0400

    Création du controller de Livre

---
+++ controllers/LivresController.php
@@ -0,0 +1,28 @@
+<?php
+
+
+/**
+ * Database configuration
+ * 
+ * PHP version 7.4
+ * 
+ * @category Config
+ * @package  Config
+ * @date     2021-01-22
+ * @version  1.00
+ * Last revision 2021-01-22
+ * 
+ */
+
+
+require_once 'config/database.php'; //On inclut le fichier database.php pour pouvoir utiliser la classe Database
+require_once 'models/Livre.php'; //On inclut le fichier Livre.php pour pouvoir utiliser la classe Livre
+
+class LivresController{
+
+    private $conn; //Variable qui va contenir la connexion à la base de données
+
+    
+
+    
+}
